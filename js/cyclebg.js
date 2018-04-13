7427f4beebd0eeb33fc16bffa7cc550852b72fd0
Louis Blake <louishuertablake@gmail.com>
2018-04-12 20:03:43 -0400

    Hullo plis

---
+++ js/cyclebg.js
@@ -0,0 +1,21 @@
+
+function cyclebg(){
+    var whichbg = 0;
+    
+    $imagesel = $('.toggle-bg');
+
+    setInterval(function() {
+
+        whichbg = whichbg + 1 < $imagesel.length ? whichbg + 1 : 0;
+
+        $imagesel.eq(whichbg).addClass("show");
+        $imagesel.eq(whichbg - 1).removeClass("show");
+    }, 9000);
+
+};
+
+$(function() {
+    cyclebg();
+
+});
+
